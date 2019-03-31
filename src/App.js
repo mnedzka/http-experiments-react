603c16c4ee554046d2e4176caee0d628ebdc31b3
Michal Nedzka <michael.nedzka@gmail.com>
2019-03-31 16:02:34 +0200

    Getting and creating data with fake API

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+const apiEndpoint = 'https://jsonplaceholder.typicode.com/posts';
 
 class App extends Component {
   state = {
-    posts: []
+    posts: [],
   };
 
-  handleAdd = () => {
-    console.log("Add");
+  async componentDidMount() {
+    const { data: posts } = await axios.get(apiEndpoint);
+    this.setState({ posts });
+  }
+
+  handleAdd = async () => {
+    const obj = { title: 'a', body: 'b' };
+
+    const { data: post } = await axios.post(apiEndpoint, obj);
+    const posts = [post, ...this.state.posts];
+    this.setState({ posts });
   };
 
   handleUpdate = post => {
-    console.log("Update", post);
+    console.log('Update', post);
   };
 
   handleDelete = post => {
-    console.log("Delete", post);
+    console.log('Delete', post);
   };
 
   render() {
     return (
       <React.Fragment>
-        <button className="btn btn-primary" onClick={this.handleAdd}>
+        <button className='btn btn-primary' onClick={this.handleAdd}>
           Add
         </button>
-        <table className="table">
+        <table className='table'>
           <thead>
             <tr>
               <th>Title</th>
@@ -37,18 +49,12 @@ class App extends Component {
               <tr key={post.id}>
                 <td>{post.title}</td>
                 <td>
-                  <button
-                    className="btn btn-info btn-sm"
-                    onClick={() => this.handleUpdate(post)}
-                  >
+                  <button className='btn btn-info btn-sm' onClick={() => this.handleUpdate(post)}>
                     Update
                   </button>
                 </td>
                 <td>
-                  <button
-                    className="btn btn-danger btn-sm"
-                    onClick={() => this.handleDelete(post)}
-                  >
+                  <button className='btn btn-danger btn-sm' onClick={() => this.handleDelete(post)}>
                     Delete
                   </button>
                 </td>
