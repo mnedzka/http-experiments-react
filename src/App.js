c24ae401a034f7b83eb6354a095043403304f3a7
Michal Nedzka <michael.nedzka@gmail.com>
2019-04-01 21:02:51 +0200

    Optimistic delete with simple error handling

--- src/App.js
+++ src/App.js
@@ -33,8 +33,17 @@ class App extends Component {
     this.setState({ posts });
   };
 
-  handleDelete = post => {
-    console.log('Delete', post);
+  handleDelete = async post => {
+    const originalPosts = this.state.posts;
+    const posts = this.state.posts.filter(p => p.id !== post.id);
+    this.setState({ posts });
+
+    try {
+      await axios.delete(apiEndpoint + '/' + post.id);
+    } catch (e) {
+      alert('Something failes when deleting a post');
+      this.setState({ posts: originalPosts });
+    }
   };
 
   render() {
